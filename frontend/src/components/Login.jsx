72b4f6e773233c578a95321e28e88bdbb73d4ff1
CodeXMalinga <rdimalingadesilva@gmail.com>
2023-02-24 16:54:49 +0530

    Log in UI Completed

--- frontend/src/components/Login.jsx
+++ frontend/src/components/Login.jsx
@@ -25,25 +25,22 @@ export default function Example() {
         <body class="h-full">
         ```
       */}
-      <div className='flex min-h-full items-center justify-center py-12 px-4 sm:px-6 lg:px-8'>
+      <div
+        id='login-page'
+        className='flex min-h-full items-center justify-center py-12 px-4 sm:px-6 lg:px-8'
+      >
         <div className='w-full max-w-md space-y-8'>
           <div>
             <img
-              className='mx-auto h-12 w-auto'
-              src='https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600'
+              className='mx-auto h-[100px] w-auto'
+              src='https://i.ibb.co/nmsH0mQ/45199.jpg'
               alt='Your Company'
             />
             <h2 className='mt-6 text-center text-3xl font-bold tracking-tight text-gray-900'>
-              Sign in to your account
+              Patient Queue Management System
             </h2>
-            <p className='mt-2 text-center text-sm text-gray-600'>
-              Or{' '}
-              <a
-                href='#'
-                className='font-medium text-indigo-600 hover:text-indigo-500'
-              >
-                start your 14-day free trial
-              </a>
+            <p className='mt-2 text-center text-xl text-gray-600'>
+              Log in to your account
             </p>
           </div>
           <form className='mt-8 space-y-6' action='#' method='POST'>
@@ -60,7 +57,7 @@ export default function Example() {
                   autoComplete='email'
                   required
                   className='relative block w-full appearance-none rounded-none rounded-t-md border border-gray-300 px-3 py-2 text-gray-900 placeholder-gray-500 focus:z-10 focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm'
-                  placeholder='Email address'
+                  placeholder='Username or Email'
                 />
               </div>
               <div>
@@ -98,7 +95,7 @@ export default function Example() {
               <div className='text-sm'>
                 <a
                   href='#'
-                  className='font-medium text-indigo-600 hover:text-indigo-500'
+                  className='font-medium text-black hover:text-indigo-500'
                 >
                   Forgot your password?
                 </a>
@@ -108,7 +105,7 @@ export default function Example() {
             <div>
               <button
                 type='submit'
-                className='group relative flex w-full justify-center rounded-md border border-transparent bg-indigo-600 py-2 px-4 text-sm font-medium text-white hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2'
+                className='group relative flex w-full justify-center rounded-md border border-transparent bg-indigo-600 py-2 px-4 text-lg font-medium text-white hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2'
               >
                 <span className='absolute inset-y-0 left-0 flex items-center pl-3'>
                   <LockClosedIcon
@@ -116,7 +113,7 @@ export default function Example() {
                     aria-hidden='true'
                   />
                 </span>
-                Sign in
+                Log in
               </button>
             </div>
           </form>
--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -5,7 +5,11 @@ module.exports = {
     "./src/**/*.{js,ts,jsx,tsx}"
   ],
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage: {
+        'loginpage': "url('/images/bgimage.jpg')",
+      }
+    },
   },
   plugins: [
     require('@tailwindcss/forms'),
